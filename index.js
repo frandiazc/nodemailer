24ce20485c358f357fdcd290fba82ec468cd73e0
frandiazc <franciscodiazcenteno@gmail.com>
2022-06-07 13:36:20 +0200

    sdfsdf
    sdfdsf

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
-alert("hola");
+let users = [
+    {
+        id: 1,
+        name: 'John'},
+    {
+        id: 2,
+        name: 'Sara'},
+    ]
+    app.get('/', (req, res) => {
+        res.send('Hello World!');
+    });
+    app.get('/users', (req, res) => {
+        res.json(users);
+    });
+    const PORT = process.env.PORT || 3001;
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
