2bbfe9238a901e522c5006f8107cd3b9b17a8e63
frandiazc <franciscodiazcenteno@gmail.com>
2022-06-07 11:48:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,2 @@
-const telegrambot = require('node-telegram-bot-api');
-const request = require('request');
-const fs = require('fs');
-const token = '5395818951:AAFIapyMNBhN1Tj6fLoGucqrOjxv2aEe4T8';
-const bot = new telegrambot(token, {polling: true});
-bot.onText(/\/start/, (msg, match) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'Iniciado');
-});
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'Received your message');
-});
+
 alert("hola");
